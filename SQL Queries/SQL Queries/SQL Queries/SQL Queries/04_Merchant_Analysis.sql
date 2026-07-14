ef21a2a4f8c3ddeff692edd32267fd0b7d328997
Oladele Oluwafeyikemi <oluwafeyikemioladele5@gmail.com>
2026-07-14 22:49:12 +0100

    Update 04_Merchant_Analysis.sql

--- SQL Queries/SQL Queries/SQL Queries/SQL Queries/04_Merchant_Analysis.sql
+++ SQL Queries/SQL Queries/SQL Queries/SQL Queries/04_Merchant_Analysis.sql
@@ -1,1 +1,57 @@
+--Top 10 Merchants by revenue
 
+SELECT TOP 10 
+merchant,
+ROUND(SUM(amt),2) AS Total_Revenue
+FROM credit_card_transactions
+GROUP BY merchant
+ORDER BY Total_Revenue DESC
+
+--Merchants distribution
+
+
+WITH CleanMerchant AS
+(
+    SELECT
+        REPLACE(merchant, 'fraud_', '') AS Merchant,
+        amt
+    FROM credit_card_transactions
+)
+
+SELECT
+    Merchant,
+    SUM(amt) AS Total_Revenue,
+    ROUND(
+        SUM(amt) * 100.0 /
+        SUM(SUM(amt)) OVER(),
+        2
+    ) AS Revenue_Percentage
+FROM CleanMerchant
+GROUP BY Merchant
+ORDER BY Total_Revenue DESC;
+
+
+
+-- Which merchant categories generate the highest revenue?
+
+SELECT
+    category,
+    SUM(amt) AS Total_Revenue,
+    COUNT(trans_num) AS Total_Transactions
+FROM credit_card_transactions
+GROUP BY
+    category
+ORDER BY
+    Total_Revenue DESC;
+
+--Top 10 merchants by Revenue
+
+SELECT TOP (10)
+    REPLACE(merchant, 'fraud_', '') AS Merchant,
+    SUM(amt) AS Total_Revenue,
+    COUNT(trans_num) AS Total_Transactions
+FROM credit_card_transaction
+GROUP BY
+    REPLACE(merchant, 'fraud_', '')
+ORDER BY
+    Total_Revenue DESC;
